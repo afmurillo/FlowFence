7aaa7e3c7680767390adae9991f2794b4708d2d2
root <root@teste2.gta.ufrj.br>
2014-08-27 16:37:44 -0300

    Added script to clear queues in bash

---
+++ clear_queues.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Queues and qos must be explicitly deleted, this script performs that.
+# WARNING! The flows associated to the qos bridge are also deletted!!!
+
+ovs-vsctl -- clear port $1 qos
+
+qosid=$(ovs-vsctl list qos | grep _uuid |  awk '{print $3;}')
+queueid=$(ovs-vsctl list queue | grep _uuid |  awk '{print $3;}')
+
+ovs-vsctl -- destroy qos $qosid
+ovs-vsctl -- destroy queue $queueid
+
