dd16586419befcab10c5cd4f814fdd89a8b42d47
andres <andres@junin>
2015-02-13 02:23:08 -0500

    fixing code

--- FlowFence_2.py
+++ FlowFence_2.py
@@ -1,84 +1,76 @@
+"""
+DoS Mitigation - FlowFence component
+
+Module running in the SDN controller
+Receives congestion notifications, calculates bandwidth for each flow, sends commands to apply bw
+"""
+
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr, IPAddr
-from collections import namedtuple
-from pox.topology.topology import Switch, Entity
 from pox.lib.revent import EventMixin
-from pox.lib.recoco import Timer
 
-import pox.lib.packet as pkt
 
 from pox.openflow.of_json import *
 
 from threading import Thread
-from collections import deque
 
-import os
-import string, sys, socket, json, subprocess
-import thread 
-import time
+import socket
+import json
 import math
 
-"""
-DoS Mitigation - FlowFence component
-"""
+LOG = core.getLogger()
+CONTROLLER_IP = '10.1.4.1'
 
-#######################################
-
-log = core.getLogger()
-controllerIp = '10.1.4.1' 
-
-############################	Here is the socket server ####
 class server_socket(Thread):
-	
+
+	""" Class that listens for switch messages """
 	def __init__(self, connections):
 		Thread.__init__(self)
-		global received
 		self.sock = None
-		#self.status =-1			
 		self.connections = connections						#dpdi of the switch
 
 	def run(self):
-		self.sock = socket.socket()         				# Create a socket object
-		host = controllerIp									
+		self.sock = socket.socket()
+		host = CONTROLLER_IP
 		port = 12345               							# Reserve a port for own communication btwn switches and controller
 		#log.info("Binding to listen for switch messages")
 		print "Binding to listen for switch messages"
 		self.sock.bind((host, port))        				# Bind to the port
-		self.sock.listen(5)                	 				# Now wait for client connection.
+		self.sock.listen(5)
 
-		while True:												
-			try:			
-				client, addr = self.sock.accept()				# Establish connection with client
-				data = client.recv(4096)						# Get data from the client 
-				print 'Message from', addr 						# Print a message confirming 
-				data_treatment = handle_message(data,self.connections, addr)	# Call the thread to work with the data received
-				data_treatment.setDaemon(True)					# Set the thread as a demond
-				data_treatment.start()							# Start the thread
+		while True:
+			try:
 
+				client, addr = self.sock.accept()
+				data = client.recv(4096)
+				print 'Message from', addr
+				data_treatment = handle_message(data,self.connections, addr)
+				data_treatment.setDaemon(True)
+				data_treatment.start()
 			except KeyboardInterrupt:
 				print "\nCtrl+C was hitten, stopping server"
 				client.close()
 				break
-		
-#########################
+
 class handle_message(Thread):
+
+	""" Handles messages sent by SDN switchhes """
 
 	def __init__(self,received,connections, addr):
 		Thread.__init__(self)
 		self.received = received
 		self.myconnections = connections
-		self.srcAddress = addr[0]
-		self.alfa=1
-		self.responsePort=23456
-		self.bwForNewFlows=0.1
+		self.src_address = addr[0]
+		self.alfa = 1
+		self.response_port = 23456
+		self.bw_for_new_flows = 0.1
 		print self.myconnections
 
 	def run(self):
-	
-		print 'message from ' + str(self.srcAddress)
+
+		print 'message from ' + str(self.src_address)
 
 		try:
 			message = eval(json.loads(self.received))
@@ -86,43 +78,36 @@ class handle_message(Thread):
 		except:
 			print "An error ocurred processing the incoming message"
 
-		#print "Type of message " + str(message['Notification'])
-	
 		if message['Notification'] == 'Congestion':
-			self.handleCongestionNotification(message['Interface']['dpid'],message, self.srcAddress)
+			self.handle_congestion_notification(message['Interface']['dpid'], message, self.src_address)
 		elif message['Notification'] == 'QueuesDone':
-			self.handleFlowsRedirection(message['Interface']['dpid'],self.myconnections, self.srcAddress, message)
+			self.handleFlowsRedirection(message['Interface']['dpid'], self.myconnections, self.src_address, message)
 
-	def handleCongestionNotification(self, dpid, notificationMessage, switchAddres):
-		# Algorithm: Classify good and bad flows, assign bandwidth fo good flows, assign band fo bad flows, assign remaining band
-		# Bad flows bw: assignedBw(j,i)=avaliableBw/badFlows - (1 -exp( - (rates(i)-capacityOverN) ) )*alfas(j)*rates(i);
-
-		flowBwList=[]
-		badFlows=0
-		bwForBadFlows=0
+	def handle_congestion_notification(self, dpid, notification_message):
+		""" Upon reception of a congestion notification, requests for flow stats in the congestioned switch """
 
                 dpid = dpid[:len(dpid)-1]
                 dpid = dpid[len(dpid)-12:]
                 print 'Received dpid: ' + str(dpid)
 
 		# We leave the 10% to handle new flows, during congestion.
-		remainingBw = notificationMessage['Interface']['capacity']*(1-self.bwForNewFlows)
 
 		# Request flow stats from switch
 		print "dpid parameter: " + str(dpid)
 		for connection in self.myconnections:
-			connectionDpid=connection.dpid
-			print "Connection dpid: " + str(connectionDpid)
-			dpidStr=dpidToStr(connectionDpid)
-			dpidStr=dpidStr.replace("-", "")
-			print 'Real dpidStr: ' + dpidStr
-			if dpid == dpidStr:
+			connection_dpid = connection.dpid
+			print "Connection dpid: " + str(connection_dpid)
+			dpid_str = dpidToStr(connection_dpid)
+			dpid_str = dpid_str.replace("-", "")
+			print 'Real dpid_str: ' + dpid_str
+			if dpid == dpid_str:
 				connection.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
-				print 'Flow stats requets sent to: ' + str(connection)				
-		
-	def handleFlowsRedirection(self, dpid, connections, switchAddress, message):
+				print 'Flow stats requets sent to: ' + str(connection)
 
-		print 'message from ' + str(switchAddress)
+	def handleFlowsRedirection(self, dpid, connections, switch_addresss, message):
+
+		""" Sends flow mod messages to redirect flows to created queues """
+		print 'message from ' + str(switch_addresss)
 		print 'Connections ' + str(dir(connections))
 
 		dpid = dpid[:len(dpid)-1]
@@ -132,15 +117,15 @@ class handle_message(Thread):
 		print "message to be used for redirection" + str(message)
 
 		msg = of.ofp_flow_mod(command=of.OFPFC_DELETE)
-		msg.priority=65535
+		msg.priority = 65535
 
 		print "dpid parameter: " + str(dpid)
 		for connection in connections:
-			connectionDpid=connection.dpid
-			dpidStr=dpidToStr(connectionDpid)
-			dpidStr=dpidStr.replace("-", "")
-			print 'Real dpidStr: ' + dpidStr
-			if dpid == dpidStr:
+			connection_dpid = connection.dpid
+			dpid_str = dpidToStr(connection_dpid)
+			dpid_str = dpid_str.replace("-", "")
+			print 'Real dpid_str: ' + dpid_str
+			if dpid == dpid_str:
 				connection.send(msg)
 				print 'Sent to: ' + str(connection)
 				print 'Well...done'
@@ -150,53 +135,49 @@ class handle_message(Thread):
 		for i in range(len(message['bwList'])):
 
 			# We only want to redirect outgoing flows
-			if message['bwList'][i]['action'] != 'OFPP_LOCAL':		
-				
+			if message['bwList'][i]['action'] != 'OFPP_LOCAL':
 
 				my_match = of.ofp_match(dl_type = 0x800,nw_src=message['bwList'][i]['nw_src'],nw_dst=message['bwList'][i]['nw_dst'])
 
 				print "Flow Match: " + str(my_match)
 				msg = of.ofp_flow_mod()
 				msg.match = my_match
-				msg.priority=65535
+				msg.priority = 65535
 				msg.idle_timeout = 0
-    				msg.hard_timeout = 0			
-
-				# There is a bug here, the error it shows reads "can't convert argument to int" when try to send the message
-				# If the actions are omitted (aka we order to drop the packets with match, we get no error)
+    				msg.hard_timeout = 0
 				msg.actions.append(of.ofp_action_enqueue(port=int(message['bwList'][i]['action']), queue_id=int(message['QueueList'][i]['queueId'])))
-				
+
 				print "Flow mod message: " + str(msg)
 
 	  	              	#toDo: Check a better way to do this
 				print "dpid parameter: " + str(dpid)
 	                	for connection in connections:
-        	        		connectionDpid=connection.dpid
-					print "Connection dpid: " + str(connectionDpid)
-                			dpidStr=dpidToStr(connectionDpid)
-	                		dpidStr=dpidStr.replace("-", "")
-        	        		print 'Real dpidStr: ' + dpidStr
+        	        		connection_dpid=connection.dpid
+					print "Connection dpid: " + str(connection_dpid)
+                			dpid_str=dpidToStr(connection_dpid)
+	                		dpid_str=dpid_str.replace("-", "")
+        	        		print 'Real dpid_str: ' + dpid_str
 
-	                		if dpid == dpidStr:
+	                		if dpid == dpid_str:
         	        			connection.send(msg)
 						print 'Sent to: ' + str(connection)
-						print 'Well...done'	
+						print 'Well...done'
 
+class connect_test(EventMixin):
 
-class connect_test(EventMixin):	
-	# Waits for OpenFlow switches to connect and makes them learning switches.
-	#Global variables of connect_test subclass
-	#global received
+	""" Waits for OpenFlow switches to connect and makes them learning switches. """
+
 	def __init__(self):
 		self.listenTo(core.openflow)
-		log.debug("Received connection from switch")
-		print("Received connection from switch")
-		self.myconnections=[]		# a list of the connections
-		socket_server=server_socket(self.myconnections)	# send it to the socket with the connection 
+		LOG.debug("Received connection from switch")
+		print "Received connection from switch"
+		self.myconnections = []		# a list of the connections
+		socket_server=server_socket(self.myconnections)	# send it to the socket with the connection
 		socket_server.setDaemon(True)		# establish the thread as a deamond, this will make to close the thread with the main program
 		socket_server.start()				# starting the thread
 
-	def _handle_ConnectionUp (self, event):
+	def _handle_ConnectionUp(self, event):
+		""" Event that handles connection events """
 		print "switch dpid " + str(event.dpid) #it prints the switch connection information, on the screen
 		print "Hex dpid: " + str(dpidToStr(event.dpid))
 		self.myconnections.append(event.connection)	# will pass as a reference to above
@@ -205,129 +186,131 @@ class connect_test(EventMixin):
 
 def _handle_flowstats_received (event):
 
-	flowList = flow_stats_to_list(event.stats)
-	sendingDpid = event.connection.dpid
-	sendingAddress = event.connection.sock.getpeername()[0]
+	""" Calculates bw for each flow """
 
-	print "Sending address " + str(sendingAddress)
-	
-	# check how to dynamically do this
+	flow_list = flow_stats_to_list(event.stats)
+	sending_dpid = event.connection.dpid
+	sending_address = event.connection.sock.getpeername()[0]
 
-	badFlows=0
-	bwForBadFlows=0
-	flowBwList=[]
+	print "Sending address " + str(sending_address)
+
+	bad_flows = 0
+	bw_for_bad_flows = 0
+	flow_bw_list = []
 	capacity = 100000000
-	bwForNewFlows = 0.0
-	remainingBw = capacity*(1-bwForNewFlows)	
-	numFlows = 0
+	bw_for_new_flows = 0.0
+	remaining_bw = capacity*(1-bw_for_new_flows)
+	num_flows = 0
 	alfa = 1
-	responsePort = 23456
+	response_port = 23456
 
-	# Get indexes of flowList
-	indexesToProcess = [flowIndex for flowIndex, flow in enumerate(flowList) if str(flow['match']['nw_dst'])=='10.1.2.1/32']
+	# Get indexes of flow_list
+	indexes_to_process = [flow_index for flow_index, flow in enumerate(flow_list) if str(flow['match']['nw_dst'])=='10.1.2.1/32']
 
-	#print "Flow List indexes: " + str(indexesToProcess)
+	#print "Flow List indexes: " + str(indexes_to_process)
 
-	while len(indexesToProcess) > 0 :
+	while len(indexes_to_process) > 0 :
 
-		#print "Remaining indexes: " + str(indexesToProcess)
+		#print "Remaining indexes: " + str(indexes_to_process)
 
 		# we should add a line to ignore if nw_dst != '10.1.2.2'
 		# Get src of first flow
-		nw_src = str(flowList[indexesToProcess[0]]['match']['nw_src'])
+		nw_src = str(flow_list[indexes_to_process[0]]['match']['nw_src'])
 
 		#print "Processing flows with nw_src: " + str(nw_src)
 
-		processingIndexes = [flowIndex for flowIndex, flow in enumerate(flowList) if str(flow['match']['nw_src']) == nw_src ]
+		processing_indexes = [flow_index for flow_index, flow in enumerate(flow_list) if str(flow['match']['nw_src']) == nw_src ]
 
-		#print "Processing indexes: " + str(processingIndexes)
+		#print "Processing indexes: " + str(processing_indexes)
 
-		flowBwDict=dict.fromkeys(['nw_src','nw_dst','reportedBw','goodBehaved','bw', 'action'])
-		flowBwDict['nw_src'] = str(flowList[processingIndexes[0]]['match']['nw_src'])
-		flowBwDict['nw_dst'] = str(flowList[processingIndexes[0]]['match']['nw_dst'])
-		flowBwDict['action'] = flowList[processingIndexes[0]]['actions'][0]['port']	
+		flow_bw_dictt=dict.fromkeys(['nw_src', 'nw_dst', 'reportedBw', 'goodBehaved', 'bw', 'action'])
+		flow_bw_dictt['nw_src'] = str(flow_list[processing_indexes[0]]['match']['nw_src'])
+		flow_bw_dictt['nw_dst'] = str(flow_list[processing_indexes[0]]['match']['nw_dst'])
+		flow_bw_dictt['action'] = flow_list[processing_indexes[0]]['actions'][0]['port']
+		acc_bw = 0
 
-		accBw = 0
+		for i in range(len(processing_indexes)):
+			acc_bw = acc_bw + flow_list[processing_indexes[i]]['byte_count']
 
-		for i in range(len(processingIndexes)):
-			accBw = accBw + flowList[processingIndexes[i]]['byte_count']
-
-		flowBwDict['reportedBw'] = accBw		
-		flowBwList.append(flowBwDict)
-		numFlows = numFlows + 1
+		flow_bw_dictt['reportedBw'] = acc_bw		
+		flow_bw_list.append(flow_bw_dictt)
+		num_flows = num_flows + 1
 		
-		for i in range(len(processingIndexes)):
-			indexesToProcess.remove(processingIndexes[i])
+		for i in range(len(processing_indexes)):
+			indexes_to_process.remove(processing_indexes[i])
 		
 	# Good flows
-	for i in range(len(flowBwList)):	
-		flowBwList[i]['goodBehaved'] = classifyFlows(capacity, flowBwList[i]['reportedBw'],numFlows)	
+	for i in range(len(flow_bw_list)):	
+		flow_bw_list[i]['goodBehaved'] = classiy_flows(capacity, flow_bw_list[i]['reportedBw'], num_flows)	
 		# only for udp Iperf debugging purposes!
-		if flowBwList[i]['nw_src']=='10.1.1.3':
-			flowBwList[i]['goodBehaved'] = True
+		if flow_bw_list[i]['nw_src'] == '10.1.1.3':
+			flow_bw_list[i]['goodBehaved'] = True
 	
-		if flowBwList[i]['goodBehaved'] == True:	
-			flowBwList[i]['bw']= flowBwList[i]['reportedBw']
-			remainingBw = remainingBw -  flowBwList[i]['reportedBw']
+		if flow_bw_list[i]['goodBehaved'] == True:	
+			flow_bw_list[i]['bw'] = flow_bw_list[i]['reportedBw']
+			remaining_bw = remaining_bw -  flow_bw_list[i]['reportedBw']
 		else:
-			badFlows=badFlows+1
+			bad_flows = bad_flows+1
 
 	# Bad Flows
-	bwForBadFlows=remainingBw
-	for i in range(len(flowBwList)):	
-		if flowBwList[i]['goodBehaved'] == False:
-			flowBwList[i]['bw']= assignBwToBadBehaved(bwForBadFlows, badFlows, capacity, numFlows, flowBwList[i]['reportedBw'], alfa)
-			print "Bad behaved flow bw " +  str(flowBwList[i]['bw'])
-			remainingBw = remainingBw - flowBwList[i]['bw']
+	bw_for_bad_flows=remaining_bw
+	for i in range(len(flow_bw_list)):	
+		if flow_bw_list[i]['goodBehaved'] == False:
+			flow_bw_list[i]['bw']= assign_bw_to_bad_behaved(capacity, num_flows, flow_bw_list[i]['reportedBw'], alfa)
+			print "Bad behaved flow bw " +  str(flow_bw_list[i]['bw'])
+			remaining_bw = remaining_bw - flow_bw_list[i]['bw']
 
 	# Give remmaining bw between good flows
-	if (badFlows < numFlows):
-		extraBw = remainingBw/(numFlows - badFlows)
-	        for i in range(len(flowBwList)):
-        	        if flowBwList[i]['goodBehaved'] == True:
-                	        flowBwList[i]['bw']=  flowBwList[i]['bw'] + extraBw
-                        	#print "Good behaved flow bw: " + str(flowBwList[i]['bw'])
+	if bad_flows < num_flows:
+		extra_bw = remaining_bw/(num_flows - bad_flows)
+	        for i in range(len(flow_bw_list)):
+        	        if flow_bw_list[i]['goodBehaved'] == True:
+                	        flow_bw_list[i]['bw'] =  flow_bw_list[i]['bw'] + extra_bw
+                        	#print "Good behaved flow bw: " + str(flow_bw_list[i]['bw'])
 
-	print "Calculated Bandwidth: " + str(flowBwList)	
+	print "Calculated Bandwidth: " + str(flow_bw_list)	
 
-	queuesDict = dict.fromkeys(['Response','dpid','bwList'])
-	queuesDict['dpid'] = sendingDpid
-	queuesDict['Response'] = "Decrement"
-	queuesDict['bwList'] = flowBwList
+	queues_dict = dict.fromkeys(['Response','dpid','bwList'])
+	queues_dict['dpid'] = sending_dpid
+	queues_dict['Response'] = "Decrement"
+	queues_dict['bwList'] = flow_bw_list
 
-	responseMessage = json.dumps(str(queuesDict))
+	response_message = json.dumps(str(queues_dict))
 
-	print "Response Message sent: " + str(responseMessage)
+	print "Response Message sent: " + str(response_message)
 
-	responseSocket = createSocket()
-	sendMessage(responseSocket,sendingAddress, responsePort, responseMessage)
-	closeConnection(responseSocket)
+	response_socket = create_socket()
+	send_message(response_socket,sending_address, response_port, response_message)
+	close_connection(response_socket)
 
-def createSocket():
+def create_socket():
+	""" Creates a socket """
 	return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	
-def sendMessage(aSocket, ipAddress, port, aMessage):
-	aSocket.connect((ipAddress, port))
-	aSocket.send(aMessage)	
+def send_message(a_socket, ip_address, port, a_message):
 
-def closeConnection(aSocket):				
-	aSocket.close()
+	""" Sends a message to a SDN switch """
+	a_socket.connect((ip_address, port))
+	a_socket.send(a_message)	
 
+def close_connection(a_socket):
+	""" Closes a connection """
+	a_socket.close()
 
-
-#In further versions, other classification methods could be used
-def classifyFlows( capacity, estimatedBw, numFlows):
-	if (estimatedBw>capacity/numFlows):
+def classiy_flows( capacity, estimated_bw, num_flows):
+	""" Classifies flows """
+	if estimated_bw>capacity/num_flows:
 		return False
 	else:
 		return True
 
-def assignBwToBadBehaved( avaliableBw, numBadFlows, capacity, numTotalFlows, flowRate, alfa):		
-	return flowRate - (1 - math.exp(-(flowRate-(capacity/numTotalFlows))))*alfa*flowRate
+def assign_bw_to_bad_behaved(capacity, num_total_flows, flow_rate, alfa):		
+	""" Assigns bw to each flow """
+	return flow_rate - (1 - math.exp(-(flow_rate-(capacity/num_total_flows))))*alfa*flow_rate
 	
 def launch ():
-	#core.openflow.addListenerByName("FlowStatsReceived", listarFluxosIp)
+	""" First method called """
 	print "FlowFence launched"
 	core.registerNew(connect_test)
 	core.openflow.addListenerByName("FlowStatsReceived", _handle_flowstats_received) 
